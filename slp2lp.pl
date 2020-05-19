774a79857748d65a2c8edc55e97b30a290c6d621
Lucian Green <luciangreen@lucianacademy.com>
2020-05-19 19:56:22 +1000

    Update slp2lp.pl

--- slp2lp.pl
+++ slp2lp.pl
@@ -3,7 +3,7 @@
 /**
 slp2lp([[f1,[a,b,c,d,e],(:-),[[+,[a,b,f]],[+,[c,f,g]],[+,[d,g,h]],[=,[h,e]]]]],Algorithm2).
 
-?- [[[n,f1],[[v,a],[v,b],[v,c],[v,d],[v,e]],":-",[[[n,+],[[v,a],[v,b],[v,f]]],[[n,+],[[v,c],[v,f],[v,g]]],[[n,+],[[v,d],[v,g],[v,h]]],[[n,=],[[v,h],[v,e]]]]]]
+Algorithm2= [[[n,f1],[[v,a],[v,b],[v,c],[v,d],[v,e]],":-",[[[n,+],[[v,a],[v,b],[v,f]]],[[n,+],[[v,c],[v,f],[v,g]]],[[n,+],[[v,d],[v,g],[v,h]]],[[n,=],[[v,h],[v,e]]]]]]
 
 **/
 
@@ -18,7 +18,7 @@ slp2lp(Algorithm1,Algorithm2) :-
 symbol((:-),":-"):-!.
 symbol(Symbol,Symbol) :-!.
 
-slp2lp_variables(Name1,[v,Name1]) :- !.
+slp2lp_variables(Name1,[v,Name1]) :- atom(Name1),!.
 slp2lp_variables(Name,Name) :- !.
 
 /**
@@ -28,7 +28,8 @@ process_body(Body1,[],Body2) :-
 
 **/
 
-predicate_or_rule_name([_,_]) :- !.
+%%predicate_or_rule_name([A,B]) :- atom(A),is_list(B),!.
+predicate_or_rule_name(A) :- atom(A),!.
 
 
 process_body2([],[]):-!.%%,Body3
@@ -46,7 +47,8 @@ process_body2(Body1,Body2%%,Body3
 	process_body2(Statements1a,Body4),
         process_body2(Statements2,Body5),
     
-	append_list2([[Body3,Body4],Body5],Body2),
+	append([Body3,Body4],Body6),
+	append([[Body6],Body5],Body2),
 	!.
 
 
@@ -58,8 +60,9 @@ process_body2(Body1,Body2) :-
 
         process_body2([Statement],Body3),
         process_body2(Statements2,Body4),
-		  append_list2([[[n,not],[Body3]],Body4
-        ],Body2),
+		  append([[n,not]],Body3,Body5),
+		  append([Body5],Body4
+        ,Body2),
 
 	!.
 	
@@ -71,8 +74,10 @@ process_body2(Body1,Body2) :-
         process_body2([Statements1],Body3),
         process_body2([Statements2],Body4),
         process_body2(Statements3,Body5),
-        append_list2([[[n,or],[Body3,Body4
-        ]],Body5],Body2),
+        append(Body3,Body4,Body34),
+        Body6=[[n,or],Body34
+        ],
+        append([Body6],Body5,Body2),
         !.
 
 
@@ -82,8 +87,10 @@ process_body2(Body1,Body2) :-
     	  process_body2([Statements2],Body4),
 
         process_body2(Statements3,Body5),
-        append_list2([[[n,"->"],[Body3,Body4
-        ]],Body5],Body2),
+        append(Body3,Body4,Body34),
+        Body6=[[n,"->"],Body34
+        ],
+        append([Body6],Body5,Body2),
 
         !.
 
@@ -96,15 +103,16 @@ process_body2(Body1,Body2) :-
         process_body2([Statements2],Body4),
         process_body2([Statements2a],Body5),
         process_body2(Statements3,Body6),
-        append_list2([[[n,"->"],[Body3,Body4,Body5
-        ]],Body6],Body2),
+        append_list2([Body3,Body4,Body5],Body345),
+        Body7=[[n,"->"],Body345],        
+        append([Body7],Body6,Body2),
 
         !.
 
 
 process_body2(Body1,Body2) :-
 	Body1=[Statement|Statements],
-	predicate_or_rule_name(Statement),
+	not(predicate_or_rule_name(Statement)),
 	process_statement1(Statement,Result1),
 	process_body2(Statements,Result2),
    append_list2([Result1,Result2],Body2),!.
@@ -113,7 +121,7 @@ process_body2(Body1,Body2) :-
 process_statement1(Statement,Result1) :-
 	Statement=[Name,Arguments],
 	findall(Name2,(member(Argument,Arguments),
-	(Argument=Name1->Name2=[v,Name1];
+	(atom(Argument)->Name2=[v,Argument];
 	Name2=Argument)),Result2),
 	Result1=[[[n,Name],Result2]].
 	
